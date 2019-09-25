648bc47327a79bff69bdee408c5943488261a9e7
Andriu <andriu_coelho1993@hotmail.com>
2019-09-25 10:25:33 -0300

    #2 Aula

---
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+let nome = "churros"
+let felicidade: Int = 5
+let calorias: Double = 79.5
+let vegatal: Bool = false
+
+
+//func alimentoConsumido() {
+//    print("o alimento consumido foi: \(nome)")
+//}
+
+//alimentoConsumido()
+//alimentoConsumido()
+
+func alimentoConsumido(nome: String, caloria: Double) {
+    print("o alimento consumido foi: \(nome), com calorias: \(caloria)")
+}
+
+alimentoConsumido(nome: nome, caloria: calorias)
+
+
+
+
