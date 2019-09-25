b69ccd9ec190cbf70d739c79265b58003141c562
Andriu <andriu_coelho1993@hotmail.com>
2019-09-25 10:21:48 -0300

    #1 Aula

---
+++ eggplant-brownie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Alura on 23/02/19.
+//  Copyright © 2019 Alura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func adicionar(_ sender: Any) {
+        let nome = "churros"
+        let felicidade = "5"
+
+        print("comi \(nome) e fiquei com felicidade: \(felicidade)")
+    }
+}
+
